59a81a046b3df67992c2203fbae4867a1dd4fab6
rishikumar08 <77142634+rishikumar08@users.noreply.github.com>
2022-05-28 13:01:29 +0530

    test database implementation

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,44 +1,21 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 from app import schemas
-from app.main import app
-from app import schemas
-from app.config import settings
-from app.database import get_db
+from .database import client, session
 
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:rishi1194420@localhost:5432/fastapi_test'
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = override_get_db
-
-
-client = TestClient(app)
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     print(res.json().get('message'))
     assert res.json().get('message') == 'Welcome to my api'
     assert res.status_code == 200
 
-def test_create_user():
-    res = client.post("/users/", json={"email": "hello12@gmail.com", "password": "password123"})
+def test_create_user(client):
+    res = client.post("/users/", json={"email": "hello@gmail.com", "password": "password123"})
     new_user = schemas.UserOut(**res.json())
-    assert new_user.email == "hello12@gmail.com"
+    assert new_user.email == "hello@gmail.com"
     assert res.status_code == 201
+
+def test_login_user(client):
+        res = client.post("/login", data={"username": "hello@gmail.com", "password": "password123"})
+        print(res.json())
+        assert res.status_code == 200
 
 
